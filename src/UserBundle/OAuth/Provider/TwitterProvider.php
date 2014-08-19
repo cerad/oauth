bcca8fe93ed015b340e03f819713ea3f8a88aa10
ahundiak <ahundiak@spike.IGSLAN.com>
2014-08-19 15:34:57 -0500

    oauth1 twitter development

--- src/UserBundle/OAuth/Provider/TwitterProvider.php
+++ src/UserBundle/OAuth/Provider/TwitterProvider.php
@@ -3,6 +3,7 @@
 namespace Cerad\Bundle\UserBundle\OAuth\Provider;
 
 use GuzzleHttp\Client;
+use HWI\Bundle\OAuthBundle\Security\OAuthUtils;
 
 /* ================================================================
  * Twitter basically does not support user login via oauth2 - very sad
@@ -10,7 +11,7 @@ use GuzzleHttp\Client;
  * 
  * See how far we can get by stealing hwi code.
  */
-class TwitterProvider
+class TwitterProvider extends AbstractProvider
 {
     protected $name = 'twitter';
     
@@ -32,8 +33,163 @@ class TwitterProvider
     }
     public function getName() { return $this->name; }
     
-    public function getAuthorizationUrl($callbackUri,$state = 'SomeGithubState')
+    /* ==================================================================
+     * Direct copy of HWI\Bundle\OAuthBundle\Security\OAuthUtils::signRequest
+     */
+    protected function httpRequest($url, $content = null, $parameters = array(), $headers = array(), $method = null)
     {
+        foreach ($parameters as $key => $value) {
+            $parameters[$key] = $key . '="' . rawurlencode($value) . '"';
+        }
+/*
+        if (!$this->options['realm']) {
+            array_unshift($parameters, 'realm="' . rawurlencode($this->options['realm']) . '"');
+        }
+*/
+        $headers[] = 'authorization: OAuth ' . implode(', ', $parameters);
+
+        return parent::httpRequest($url, $content, $headers, $method);
+    }
+
+    protected function signRequest($method,$urlInput,$params,$clientSecret,$tokenSecret = '')
+    {
+        // Validate required parameters
+        foreach (array('oauth_consumer_key','oauth_timestamp','oauth_nonce','oauth_signature_method','oauth_version') as $paramName) {
+            if (!isset($params[$paramName])) {
+                throw new \RuntimeException(sprintf('OAUTH1 SignRequest Parameter "%s" must be set.', $paramName));
+            }
+        }
+        // Remove oauth_signature if present
+        // Ref: Spec: 9.1.1 ("The oauth_signature parameter MUST be excluded.")
+        if (isset($params['oauth_signature'])) {
+            unset($params['oauth_signature']);
+        }
+        // Parse & add query params as base string parameters if they exists
+        $queryParams = array();
+        $urlParts = parse_url($urlInput);
+        if (isset($urlParts['query'])) {
+            parse_str($urlParts['query'], $queryParams);
+            $params += $queryParams;
+        }
+        // Remove default ports
+        // Ref: Spec: 9.1.2
+        $explicitPort = isset($urlParts['port']) ? $urlParts['port'] : null;
+        if (('https' === $urlParts['scheme'] && 443 === $explicitPort) || 
+            ( 'http' === $urlParts['scheme'] &&  80 === $explicitPort)) {
+            $explicitPort = null;
+        }
+        // Remove query params from URL
+        // Ref: Spec: 9.1.2
+        $urlGenerated = sprintf('%s://%s%s%s', 
+            $urlParts['scheme'], 
+            $urlParts['host'  ], 
+           ($explicitPort ? ':'.$explicitPort : ''), 
+            isset($urlParts['path']) ? $urlParts['path'] : ''
+        );
+        // Parameters are sorted by name, using lexicographical byte value ordering.
+        // Ref: Spec: 9.1.1 (1)
+        uksort($params, 'strcmp');
+        // http_build_query should use RFC3986
+        
+        $parts = array(
+            // HTTP method name must be uppercase
+            // Ref: Spec: 9.1.3 (1)
+            strtoupper($method),
+            rawurlencode($urlGenerated),
+            rawurlencode(str_replace(array('%7E', '+'), array('~', '%20'), http_build_query($params, '', '&'))),
+        );
+        $baseString = implode('&', $parts);
+        
+        // HMAC
+        $keyParts = array(rawurlencode($clientSecret),rawurlencode($tokenSecret));
+        
+        $signature = hash_hmac('sha1', $baseString, implode('&', $keyParts), true);
+        
+        return base64_encode($signature);
+    }
+    protected function getRequestToken($callbackUri, $state)
+    {
+        $timestamp = time();
+
+        // https://dev.twitter.com/docs/api/1/post/oauth/request_token
+        // http://tools.ietf.org/html/rfc5849
+        $params = array(
+            'oauth_consumer_key'     => $this->clientId,
+            'oauth_timestamp'        => $timestamp,
+            'oauth_nonce'            => $state,
+            'oauth_version'          => '1.0', // Optional
+            'oauth_callback'         => $callbackUri,
+            'oauth_signature_method' => 'HMAC-SHA1',
+        );
+        $sig = OAuthUtils::signRequest('POST',$this->requestTokenUrl,$params,$this->clientSecret,'','HMAC-SHA1');
+        echo 'Sig 1 ' . $sig . '<br />';
+        
+        $params['oauth_signature'] = 
+            $this->signRequest('POST',$this->requestTokenUrl,$params,$this->clientSecret);
+        
+        echo 'Sig 3 ' . $params['oauth_signature'] . '<br />';
+        
+        $apiResponse = $this->httpRequest($this->requestTokenUrl, null, $params, array(), 'POST');
+        $responseInfo = $this->getResponseContent($apiResponse);
+        print_r($responseInfo); die();
+        // Make the Auth
+        foreach($params as $key => $value) {
+            $params[$key] = $key . '="' . rawurlencode($value) . '"';
+        }
+        
+        // Headers
+        $headers = array(
+            'User-Agent: HWIOAuthBundle (https://github.com/hwi/HWIOAuthBundle)',
+            'Content-Length: ' . 0,
+            'Authorization: OAuth ' . implode(', ', $params),
+        );
+        print_r($headers); echo '<br />';
+        $client = new Client();
+        
+        try{
+            $response = $client->post($this->requestTokenUrl,array(
+                'headers' => $headers,
+                'body' => null,
+            ));
+        }
+        catch (\Exception $e)
+        {
+            die('Response Exception ' . $e->getMessage());
+        }
+        $responseData = $response->json();
+        
+        if (is_array($responseData)) print_r($responseData);
+        
+        die('getRequestToken');
+
+        $apiResponse = $this->httpRequest($url, null, $parameters, array(), HttpRequestInterface::METHOD_POST);
+die('request token' . $url);
+
+        $response = $this->getResponseContent($apiResponse);
+
+        if (isset($response['oauth_problem'])) {
+            throw new AuthenticationException(sprintf('OAuth error: "%s"', $response['oauth_problem']));
+        }
+
+        if (isset($response['oauth_callback_confirmed']) && ($response['oauth_callback_confirmed'] != 'true')) {
+            throw new AuthenticationException('Defined OAuth callback was not confirmed.');
+        }
+
+        if (!isset($response['oauth_token']) || !isset($response['oauth_token_secret'])) {
+            throw new AuthenticationException('Not a valid request token.');
+        }
+
+        $response['timestamp'] = $timestamp;
+
+        $this->storage->save($this, $response);
+
+        return $response;        
+    }
+    public function getAuthorizationUrl($callbackUri,$state = 'SomeTwitterState')
+    {
+        $requestToken = $this->getRequestToken($callbackUri,$state);
+        die('Twitter requestToken ' . $requestToken);
+        
         $params = array(
             'response_type' => 'code',
             'client_id'     => $this->clientId,
