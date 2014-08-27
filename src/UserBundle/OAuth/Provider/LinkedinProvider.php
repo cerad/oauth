2c9afc86db3ebaa875d3df124446190bd750cd64
Art (giles) Hundiak <ahundiak@gmail.com>
2014-08-27 05:41:02 -0500

    Providers

--- src/UserBundle/OAuth/Provider/LinkedinProvider.php
+++ src/UserBundle/OAuth/Provider/LinkedinProvider.php
@@ -14,82 +14,45 @@ use GuzzleHttp\Client;
  * 
  * Array ( [emailAddress] => ahundiak@ayso894.org [formattedName] => Art Hundiak [id] => 2jSjTge1i1 )
  */
-class LinkedinProvider
+class LinkedinProvider extends AbstractProvider
 {
-    protected $name = 'linkedin';
-    
-    protected $clientId;
-    protected $clientSecret;
-    
-    protected $scope = null;
-    protected $userProfileUrl   = 'https://api.linkedin.com/v1/people/~:(id,formatted-name,email-address,picture-url)';
+    protected $userInfoUrl      = 'https://api.linkedin.com/v1/people/~:(id,formatted-name,email-address,picture-url)';
     protected $accessTokenUrl   = 'https://www.linkedin.com/uas/oauth2/accessToken';
     protected $authorizationUrl = 'https://www.linkedin.com/uas/oauth2/authorization';
     
-    public function __construct($clientId,$clientSecret)
+    public function getUserInfo($accessToken)
     {
-        $this->clientId         = $clientId;
-        $this->clientSecret     = $clientSecret;
-    }
-    public function getName() { return $this->name; }
-    
-    public function getAuthorizationUrl($callbackUri,$state = 'SomeGithubState')
-    {
-        $params = array(
-            'response_type' => 'code',
-            'client_id'     => $this->clientId,
-            'scope'         => $this->scope,
-            'redirect_uri'  => $callbackUri,
-            'state'         => $state,
-        );
-        return $this->authorizationUrl . '?' . http_build_query($params);
-    }
-    public function getAccessTokenUrl()
-    {
-        return $this->accessTokenUrl;
-    }
-    public function getAccessTokenQuery($code,$callbackUri)
-    {
-        $accessTokenQuery = array(
-            'grant_type'    => 'authorization_code',
-            'code'          => $code,
-            'client_id'     => $this->clientId,
-            'client_secret' => $this->clientSecret,
-            'redirect_uri'  => $callbackUri,
-        );
-        return $accessTokenQuery;
-    }
-    public function getAccessToken($code,$callbackUri)
-    {
-        $client = new Client();
-        
-        $response = $client->post($this->accessTokenUrl,array(
-            'headers' => array('Accept' => 'application/json'),
-            'body' => $this->getAccessTokenQuery($code,$callbackUri)
-        ));
-        $responseData = $response->json();
-        
-        return $responseData['access_token'];
-    }
-    public function getUserProfileUrl()
-    {
-        return $this->userProfileUrl;
-    }
-    public function getUserProfile($accessToken)
-    {
-        $client = new Client();
-        
-        $response = $client->get($this->userProfileUrl,array(
+        $response = $this->client->get($this->userInfoUrl,array(
             'headers' => array(
                 'Accept' => 'application/json',
               //'Authorization'  => 'bearer ' . $accessToken,
             ),
             'query' => array(
                 'format' => 'json',
-                'oauth2_access_token' => $accessToken
+                'oauth2_access_token' => $accessToken['access_token'],
             ),
         ));
-        // TODO: Add providerName
-        return $response->json();
+        $data = $this->getResponseData($response);
+        
+        $nameParts = explode('@',$data['emailAddress']);
+        $nickname = count($nameParts) ? $nameParts[0] : null;
+        
+        $userInfo = array(
+            'identifier'     => $data['id'],
+            'nickname'       => $nickname,
+            'realname'       => $data['formattedName'],
+            'email'          => $data['emailAddress'],
+            'profilepicture' => null,
+            'providername'   => $this->name,
+        );
+        return $userInfo;        
     }
+/*
+ * Array ( 
+ * [emailAddress] => ahundiak@ayso894.org 
+ * [formattedName] => Art Hundiak 
+ * [id] => 2jSjTge1i1 
+ * )
+ */
+
 }
